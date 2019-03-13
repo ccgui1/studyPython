3a795f98cc731e42708cc42219111e96cab39a73
root <root@localhost.localdomain>
2019-03-13 20:25:00 +0800

    second

--- regex.py
+++ regex.py
@@ -25,3 +25,11 @@ print(robocop.search('RoboCop is part man,part machine,all cop.').group())
 print(robocop.search('ROBOCOP protects the innocent.').group())
 
 print(robocop.search('A1,why does your programming book talk about robocop so much?').group())
+
+namesRegex = re.compile(r'Agent \w+')
+print(namesRegex.sub('CENSORED','Agent Alice gave the secret documents to Agent Bob.'))
+
+agentNamesRegex = re.compile(r'Agent \w(\w)*')#(\w)即与下面的\1所对应.
+print(agentNamesRegex.sub(r'\1****','Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.'))
+
+#e**** told l**** that e**** knew b**** was a double agent.
