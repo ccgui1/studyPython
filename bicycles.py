564bde737d70c552efa0a73d198c7cae303c9ded
ccgui1 <1627389570@qq.com>
2019-05-11 18:53:25 +0800

    bicycles

--- bicycles.py
+++ bicycles.py
@@ -3,6 +3,23 @@ print(bicycles)
 print(bicycles[0])
 print()
 print(bicycles[0].title())
-name = "ada lovelace"
+name = "Ada lovelace"
 print(name.title())
-print 
+print(name.upper()) #大写
+print(name.lower()) #小写 
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+print("********************************")
+print("Hello, " + full_name.title() + "!")
+print("********************************")
+message = "Hello, " + full_name.title() + '!'
+print(message)
+print("********************************")
+print("\tPython")
+print("********************************")
+print("Languages:\nPython\nC\nJaveScript")
+
