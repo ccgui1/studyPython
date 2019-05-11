441714b1fe2ee6bed988f71385e74d679932ea67
ccgui1 <1627389570@qq.com>
2019-05-11 17:01:07 +0800

    bicycles

---
+++ bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
+print(bicycles[0])
+print()
+print(bicycles[0].title())
+name = "ada lovelace"
+print(name.title())
+print 
