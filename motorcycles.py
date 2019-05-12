e551fe4f9f648a3113f4812404c0ec744c1d6a4a
ccgui1 <1627389570@qq.com>
2019-05-12 11:37:10 +0800

    second motorcycles

--- motorcycles.py
+++ motorcycles.py
@@ -30,12 +30,33 @@ del motorcycles[0]
 print(motorcycles)
 
  #使用pop()方法删除元素
+print("*****************************")
 motorcycles = ['honda','yamaha','suzuki']
 print(motorcycles)
 
 popped_motorcycles = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycles)
+print("*****************************")
+last_owned = motorcycles.pop()
+print("The last motorcycles I owned was a " + last_owned.title() + '.') 
+ ##pop()可删除列表中任何位置的元素，在括号中指定位置
+print("*****************************")
+motorcycles = ['honda','yamada','suzuki']
+first_owned = motorcycles.pop(0)
+print('The first motorcycle I owned was a ' + first_owned.title() + '.')
+
+#根据值删除元素
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+
+#motorcycles.remove('ducati')
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
 
 
 
