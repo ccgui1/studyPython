8e6a8dc8e656cf0cca43de2ae1284e704bfa1f4d
ccgui1 <1627389570@qq.com>
2019-05-14 08:54:30 +0800

    class

--- bicycles.py
+++ bicycles.py
@@ -4,6 +4,7 @@ print(bicycles[0])
 print()
 print(bicycles[0].title())
 name = "Ada lovelace"
+print(bicycles[-1].title())
 print(name.title())
 print(name.upper()) #大写
 print(name.lower()) #小写 
@@ -22,4 +23,16 @@ print("********************************")
 print("\tPython")
 print("********************************")
 print("Languages:\nPython\nC\nJaveScript")
+print("********************************")
+#rstrip()剔除末尾的空白
+#lstrip()剔除开头的空白
+#strip()剔除开头和末尾的空白
+favorite_language = '   python '
+print(favorite_language)
+print(favorite_language.rstrip())
+favorite_language = favorite_language.rstrip()
+#print(favorite_language.lstrip())
+print(favorite_language.strip())
+print("********************************")
+
 
